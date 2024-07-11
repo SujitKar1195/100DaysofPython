4d028633c2773283926861afbf5f998eecfbe5fc
SujitKar1195 <sujitkar1195@gmail.com>
2024-07-11 10:36:02 +0000

    Day1 Challenge is completed

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.\n")
+print("What's the name of the city you grew up in?\n")
+cityName = input("--> ")
+print("What's your pet name?\n")
+petName = input("--> ")
+print("\n::> Your band name could be " + cityName + " " + petName + "\n")
+
